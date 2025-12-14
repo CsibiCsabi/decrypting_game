58c3dd1feca363631c2807dae462f001482a8c1c
Nick <nick.esselman@gmail.com>
2025-12-14 15:56:38 +0100

    changed up end screen

--- decrypting-game/end_screen.gd
+++ decrypting-game/end_screen.gd
@@ -1,13 +1,15 @@
 extends Control
 
+@onready var score: Label = %Score
+@onready var high_score: Label = %HighScore
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Score.text = str(Gs.points)
-	$HighScore.text = str(Gs.highScore)
+	score.text = "Score: " + str(Gs.points)
+	high_score.text = "High Score: " + str(Gs.highScore)
 
 func _on_restart_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://level.tscn")
+	get_tree().change_scene_to_file("res://ui.tscn")
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu.tscn")
